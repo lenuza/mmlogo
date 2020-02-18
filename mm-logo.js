a3198bf27f0218f6d77320114c5d56554634e541
Elena Nedkova <lenuza@gmail.com>
2020-02-18 18:44:35 +0200

    added 10 paragraphs

--- mm-logo.js
+++ mm-logo.js
@@ -1,10 +1,100 @@
 function drawLogo() {
     var n = document.getElementById('number').value;
 
-    for(var y = n + 1; y >= 0; y--) {
-        document.getElementById('canvas').innerHTML += `${('_').repeat(n)} ${('*').repeat(n)} ${('_').repeat(n)} ${('*').repeat(n)} ${('_').repeat(2*n)} ${('*').repeat(n)} ${('_').repeat(n)} ${('*').repeat(n)} ${('_').repeat(n)}`;
+    if(n%2 != 0 && n > 0 && n < 10000) {
+        document.getElementById('one').innerHTML =
+        ('-').repeat(n) + ('*').repeat(n-n) + '<br>'
+
+        + ('-').repeat(n-1) + ('*').repeat(n-2) + '<br>'
+
+        + ('-').repeat(n-2) + ('*').repeat(n-1) + '<br>'
+
+        + ('-').repeat(n-n) + ('*').repeat(n);
+
+        document.getElementById('two').innerHTML =
+        ('-').repeat(n) + ('*').repeat(n-n) + '<br>'
+
+        + ('-').repeat(n-1) + ('*').repeat(n-2) + '<br>'
+
+        + ('-').repeat(n-2) + ('*').repeat(n-1) + '<br>'
+
+        + ('-').repeat(n-n) + ('*').repeat(n);
+
+        document.getElementById('three').innerHTML =
+        ('-').repeat(n) + ('*').repeat(n-n) + '<br>'
+
+        + ('-').repeat(n-1) + ('*').repeat(n-2) + '<br>'
+
+        + ('-').repeat(n-2) + ('*').repeat(n-1) + '<br>'
+
+        + ('-').repeat(n-n) + ('*').repeat(n);
+
+        document.getElementById('four').innerHTML =
+        ('-').repeat(n) + ('*').repeat(n-n) + '<br>'
+
+        + ('-').repeat(n-1) + ('*').repeat(n-2) + '<br>'
+
+        + ('-').repeat(n-2) + ('*').repeat(n-1) + '<br>'
+
+        + ('-').repeat(n-n) + ('*').repeat(n);
+
+        document.getElementById('five').innerHTML =
+        ('-').repeat(n) + ('*').repeat(n-n) + '<br>'
+
+        + ('-').repeat(n-1) + ('*').repeat(n-2) + '<br>'
+
+        + ('-').repeat(n-2) + ('*').repeat(n-1) + '<br>'
+
+        + ('-').repeat(n-n) + ('*').repeat(n);
+
+        document.getElementById('six').innerHTML =
+        ('-').repeat(n) + ('*').repeat(n-n) + '<br>'
+
+        + ('-').repeat(n-1) + ('*').repeat(n-2) + '<br>'
+
+        + ('-').repeat(n-2) + ('*').repeat(n-1) + '<br>'
+
+        + ('-').repeat(n-n) + ('*').repeat(n);
+
+        document.getElementById('seven').innerHTML =
+        ('-').repeat(n) + ('*').repeat(n-n) + '<br>'
+
+        + ('-').repeat(n-1) + ('*').repeat(n-2) + '<br>'
+
+        + ('-').repeat(n-2) + ('*').repeat(n-1) + '<br>'
+
+        + ('-').repeat(n-n) + ('*').repeat(n);
+
+        document.getElementById('eight').innerHTML =
+        ('-').repeat(n) + ('*').repeat(n-n) + '<br>'
+
+        + ('-').repeat(n-1) + ('*').repeat(n-2) + '<br>'
+
+        + ('-').repeat(n-2) + ('*').repeat(n-1) + '<br>'
+
+        + ('-').repeat(n-n) + ('*').repeat(n);
+
+        document.getElementById('nine').innerHTML =
+        ('-').repeat(n) + ('*').repeat(n-n) + '<br>'
+
+        + ('-').repeat(n-1) + ('*').repeat(n-2) + '<br>'
+
+        + ('-').repeat(n-2) + ('*').repeat(n-1) + '<br>'
+
+        + ('-').repeat(n-n) + ('*').repeat(n);
+
+        document.getElementById('ten').innerHTML =
+        ('-').repeat(n) + ('*').repeat(n-n) + '<br>'
+
+        + ('-').repeat(n-1) + ('*').repeat(n-2) + '<br>'
+
+        + ('-').repeat(n-2) + ('*').repeat(n-1) + '<br>'
+
+        + ('-').repeat(n-n) + ('*').repeat(n);
     }
+
 }
+
 
 function deleteLogo() {
     document.getElementById('canvas').innerHTML ='';
