fb2ce75e4506006bb621cc265a08a711a2a135f8
Elena Nedkova <lenuza@gmail.com>
2020-02-19 21:16:37 +0200

    cleaned a little bit the code

--- mm-logo.js
+++ mm-logo.js
@@ -3,31 +3,27 @@ function drawLogo () {
 
     if(n%2 != 0 && n > 0 && n < 10000) {
 
-            document.getElementById('canvas').innerHTML +=
-            merge(
-                    genCompStair(n),
-                    genCompTwo(n),
-                    flipVertCompTwo(n),
-                    flipHorizontallyStair(n)
-                );
-
+        document.getElementById('canvas').innerHTML +=
+        mergeSegments(
+            genCompStair(n),
+            genCompTwo(n),
+            flipVertCompTwo(n),
+            flipHorizontallyStair(n)
+        );
     }
 }
 
 function genCompStair (n) {
-
     var stair = [];
 
     for(var y = n; y >= 0; y--) {
         stair.push(('-').repeat(n - (n - y)).concat(('*').repeat(n - y))) + '\n';
     }
 
-    console.log(stair)
     return stair;
 }
 
 function genCompTwo (n) {
-
     var compTwo = [];
 
     for(var y = n/2; y >= 0; y--) {
@@ -37,7 +33,6 @@ function genCompTwo (n) {
         compTwo.push(('*').repeat(Math.floor(n/2) - y).concat(('-').repeat(y+1)).concat(('-').repeat(y)).concat(('*').repeat(Math.floor(n/2) - y))) + '\n';
     }
 
-    console.log(compTwo)
     return compTwo;
 }
 
@@ -48,12 +43,10 @@ function flipHorizontallyStair (n) {
         flipStair.push(('*').repeat(n - y).concat(('-').repeat(n - (n - y)))) + '\n';
     }
 
-    console.log(flipStair)
     return flipStair;
 }
 
 function flipVertCompTwo (n) {
-
     var flipCompTwo = [];
 
     for(var y = 0; y <= n/2; y++) {
@@ -63,7 +56,6 @@ function flipVertCompTwo (n) {
         flipCompTwo.push(('*').repeat(n)) + '\n';
     }
 
-    console.log(flipCompTwo)
     return flipCompTwo;
 }
 
@@ -71,8 +63,7 @@ function deleteLogo() {
     document.getElementById('canvas').innerHTML ='';
 }
 
-function merge (stair, compTwo, flipCompTwo, flipStair) {
-
+function mergeSegments (stair, compTwo, flipCompTwo, flipStair) {
     var concatArrays = new Array();
 
     concatArrays[0] = stair;
@@ -86,22 +77,18 @@ function merge (stair, compTwo, flipCompTwo, flipStair) {
     concatArrays[8] = compTwo;
     concatArrays[9] = flipStair
 
-
-    // var concatArrays = [];
-
-    // concatArrays = concatArrays.concat(stair.concat(compTwo).concat(flipCompTwo).concat(flipStair));
-    console.log( concatArrays)
     return showLine(concatArrays)
 }
 
 function showLine (concatArrays) {
     var toshow = '';
+
     for (let j = 0; j < concatArrays[0].length; j++) {
         for (let i = 0; i < concatArrays.length; i++) {
             toshow += concatArrays[i][j];
         }
         toshow += '\n';
     }
-    console.log(toshow)
+
     return toshow;
 }
